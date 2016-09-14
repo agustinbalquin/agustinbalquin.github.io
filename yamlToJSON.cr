b452d7c542e3017d82b4a20256bc4407feef2a8c
agustinbalquin <agustinbalquin@gmail.com>
2016-09-14 06:59:10 -0700

    converter updated and json compiled

--- yamlToJSON.cr
+++ yamlToJSON.cr
@@ -1,6 +1,12 @@
 require "yaml"
 require "json"
+if(ARGV.size == 0)
+  puts "\tyamlToJSON.cr INPUT_DIR [OUTPUT_DIR]"
+  puts "\t- if OUTPUT_DIR is omitted then STDOUT will be used"
+  exit(0)
+end
 files = Dir.glob(File.join(File.expand_path(ARGV[0]),"*.page"))
+puts files
 pages = files.map do |f|
   begin
     YAML.parse(File.read(f)).as_h
@@ -8,4 +14,8 @@ pages = files.map do |f|
     puts "ERROR at #{f}"
   end
 end
-File.write(File.join(File.expand_path(ARGV[1]),"pages.js"), pages.to_json)
+if(ARGV.size == 1)
+  puts pages.to_json
+else
+  File.write(File.join(File.expand_path(ARGV[1]),"pages.js"), pages.to_json)
+end
